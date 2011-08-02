ce537439a9e2ac72fcd88c84b230ced20791ce07
Nigel Banks <nigel.g.banks@gmail.com>
2011-08-02 17:40:05 -0300

    Corrected the size/spacing of elements on the home page a bit.

--- templates/Home.tpl.php
+++ templates/Home.tpl.php
@@ -1,16 +1,14 @@
-<h3>Quick Search</h3>
-<br/>
+<h4>Quick Search</h4>
 <div id="ir-quick-search-form">
   <?php print drupal_get_form('scholar_quick_search_form') ?>
 </div>
 <div>or</div>
 <br/>
-<h3>Browse by name</h3>
-<br/>
+<h4>Browse by name</h4> 
 <div id="ir-browse-by-user-form">
   <?php print drupal_get_form('scholar_browse_by_user_form') ?>
 </div>
-<h3>Browse by Department</h3>
+<h4>Browse by Department</h4>
 <br/>
 <div id="ir-browse-by-department">
   <!-- Create a List -->
