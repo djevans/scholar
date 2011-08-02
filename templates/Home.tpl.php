809baeec19576b6c7ece43ea5b6399a95833c416
Nigel Banks <nigel.g.banks@gmail.com>
2011-08-02 16:53:40 -0300

    Seems as though the Quick Search form is referenced but not defined anywhere, going to have to implement it.

--- templates/Home.tpl.php
+++ templates/Home.tpl.php
@@ -1,1 +1,13 @@
-<div>Template for home page.</div>
+<h3>Quick Search</h3>
+<br/>
+<?php drupal_get_form('scholar_quick_search_form') ?>
+<div>or</div>
+<br/>
+<h3>Browse by name</h3>
+<br/>
+<?php drupal_get_form('scholar_browse_by_user_form') ?>
+<h3>Browse by Department</h3>
+<br/>
+<div>
+  <!-- Create a List -->
+</div>
