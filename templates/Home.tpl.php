f749655056577a3703ffe593089b9e28044e894f
Nigel Banks <nigel.g.banks@gmail.com>
2011-08-02 17:35:38 -0300

    Started in on theming the home page a bit.

--- templates/Home.tpl.php
+++ templates/Home.tpl.php
@@ -1,13 +1,17 @@
 <h3>Quick Search</h3>
 <br/>
-<?php drupal_get_form('scholar_quick_search_form') ?>
+<div id="ir-quick-search-form">
+  <?php print drupal_get_form('scholar_quick_search_form') ?>
+</div>
 <div>or</div>
 <br/>
 <h3>Browse by name</h3>
 <br/>
-<?php drupal_get_form('scholar_browse_by_user_form') ?>
+<div id="ir-browse-by-user-form">
+  <?php print drupal_get_form('scholar_browse_by_user_form') ?>
+</div>
 <h3>Browse by Department</h3>
 <br/>
-<div>
+<div id="ir-browse-by-department">
   <!-- Create a List -->
 </div>
