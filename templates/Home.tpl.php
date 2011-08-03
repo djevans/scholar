509b19e49848b61a9a8cf57f1be873d011e935fc
Nigel Banks <nigel.g.banks@gmail.com>
2011-08-03 13:39:51 -0300

    Renamed the search form for clarity.

--- templates/Home.tpl.php
+++ templates/Home.tpl.php
@@ -1,7 +1,7 @@
 <br/>
 <h4>Quick Search</h4>
 <div id="ir-quick-search-form">
-  <?php print drupal_get_form('scholar_quick_search_form') ?>
+  <?php print drupal_get_form('scholar_search_form') ?>
 </div>
 <?php if ($browse_by_user || $browse_by_department): ?>
   <div>or</div>
