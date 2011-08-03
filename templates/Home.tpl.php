1671301bf5242a003fc0c36dd4b2b3823540b119
Nigel Banks <nigel.g.banks@gmail.com>
2011-08-03 10:20:02 -0300

    Only show the Browse by User Section if there are users.

--- templates/Home.tpl.php
+++ templates/Home.tpl.php
@@ -1,15 +1,23 @@
+<br/>
 <h4>Quick Search</h4>
 <div id="ir-quick-search-form">
   <?php print drupal_get_form('scholar_quick_search_form') ?>
 </div>
-<div>or</div>
-<br/>
-<h4>Browse by name</h4> 
-<div id="ir-browse-by-user-form">
-  <?php print drupal_get_form('scholar_browse_by_user_form') ?>
-</div>
-<h4>Browse by Department</h4>
-<br/>
-<div id="ir-browse-by-department">
-  <!-- Create a List -->
-</div>
+<?php if ($browse_by_user || $browse_by_department): ?>
+  <div>or</div>
+  <br/>
+  <?php if ($browse_by_user): ?>
+    <h4>Browse by User</h4> 
+    <div id="ir-browse-by-user-form">
+      <?php print drupal_get_form('scholar_browse_by_user_form') ?>
+    </div>
+    <br/>
+  <?php endif; ?>
+  <?php if ($browse_by_department): ?>
+    <h4>Browse by Department</h4>
+    <br/>
+    <div id="ir-browse-by-department">
+      <?php print theme_item_list(array('stuff')); ?>
+    </div>
+  <?php endif; ?>
+<?php endif; ?>
