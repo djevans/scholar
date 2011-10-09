cae0a21d18396c770d8090496c5cfdf370f12ef9
Nigel Banks <nigel.g.banks@gmail.com>
2011-10-10 01:10:02 +0200

    Cleaned up the formatting some, added abstract to the citation.

--- templates/CitationModel.tpl.php
+++ templates/CitationModel.tpl.php
@@ -1,3 +1,12 @@
 <div id="overview">
-  <?php print print_r($citation, TRUE); ?>
+  <div id="citation">
+    <h3><?php print t('Citation:') ?></h3>
+    <?php print print_r($citation, TRUE); ?>
+  </div>
+  <?php if (isset($abstract)): ?>
+    <div id="abstract">
+      <h3><?php print t('Abstract:') ?></h3>
+      <?php print print_r($abstract, TRUE); ?>
+    </div>
+  <?php endif; ?>
 </div>
