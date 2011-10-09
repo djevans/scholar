6113c61445aee790748c9f52e873f5eeb2072105
Nigel Banks <nigel.g.banks@gmail.com>
2011-10-10 01:34:31 +0200

    Nicely formatted overview page.

--- templates/CitationModel.tpl.php
+++ templates/CitationModel.tpl.php
@@ -1,12 +1,19 @@
 <div id="overview">
   <div id="citation">
-    <h3><?php print t('Citation:') ?></h3>
+    <h3>
+      <?php print t('Citation:') ?>
+      <select name="" class="form-select">
+        <option value="0">APA</option>
+      </select>
+    </h3> 
     <?php print print_r($citation, TRUE); ?>
+    <?php print theme_button(array('#value' => 'Add To Bibliography')); ?>
   </div>
+
   <?php if (isset($abstract)): ?>
     <div id="abstract">
       <h3><?php print t('Abstract:') ?></h3>
-      <?php print print_r($abstract, TRUE); ?>
+      <div id="abstract-content"><?php print print_r($abstract, TRUE); ?></div>
     </div>
   <?php endif; ?>
 </div>
