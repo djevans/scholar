39314d2195cbacdc87f68a1d2d2bf5f1a8445cff
Nigel Banks <nigel.g.banks@gmail.com>
2011-10-10 02:52:59 +0200

    Bibliography is now working correctly.

--- templates/CitationModel.tpl.php
+++ templates/CitationModel.tpl.php
@@ -1,15 +1,14 @@
 <div id="overview">
   <div id="citation">
-    <h3>
+    <h3 style="display:inline">
       <?php print t('Citation:') ?>
-      <select class="form-select" onchange="">
-        <option value="0">APA</option>
-      </select>
     </h3> 
+    <select class="form-select" onchange="">
+      <option value="0">APA</option>
+    </select>
     <?php print print_r($citation, TRUE); ?>
-    <?php print theme_button(array('#value' => 'Add To Bibliography')); ?>
+    <?php print drupal_get_form('islandora_bibliography_citation_form', $pid); ?>
   </div>
-
   <?php if (!empty($abstract)): ?>
     <div id="abstract">
       <h3><?php print t('Abstract:') ?></h3>
