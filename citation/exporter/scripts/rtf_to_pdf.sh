70f1fa872f9f88d854726856f40d5f1b61a62bd0
Nigel Banks <nigel.g.banks@gmail.com>
2011-10-25 14:44:09 +0100

    Calling the proper command now.

--- citation/exporter/scripts/rtf_to_pdf.sh
+++ citation/exporter/scripts/rtf_to_pdf.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 clear
-echo "Testing 123"
-echo $1
-echo $2
-
-#soffice.bin --headless -convert-to pdf /sites/default/files/tmp/export_5.rtf -outdir /sites/default/files/tmp
+soffice.bin --headless -convert-to pdf $1 -outdir $2
